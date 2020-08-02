e3b1cb1d51602b806c1fb99d83ba83a3b21eb516
dnyandeobharambe <dnyandeoaz@outlook.com>
2020-08-02 15:05:01 -0700

    Function with cosmosdb

--- FunHTTPTrigger/httpFun.cs
+++ FunHTTPTrigger/httpFun.cs
@@ -7,29 +7,25 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.Documents.Client;
+using System.Linq;
+using System.Collections.Generic;
+using FunHTTPTrigger.Models;
 
 namespace FunHTTPTrigger
 {
     public static class httpFun
     {
+        private static DocumentClient client = new DocumentClient(new Uri(""), "");
+        private static Uri productCollectionUri = UriFactory.CreateDocumentCollectionUri("serverless", "products");
+        //careful in prod
+        private static readonly FeedOptions productQueryOptions = new FeedOptions { MaxItemCount = -1 };
+
+
         [FunctionName("httpFun")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static async Task<List<Product>> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return client.CreateDocumentQuery<Product>(productCollectionUri, productQueryOptions).ToList();
         }
     }
 }
